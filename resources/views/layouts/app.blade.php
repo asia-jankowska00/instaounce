ec8e78ddfe53eec2f463d6bb0cce3b2d9452dd5f
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-12-03 16:08:24 +0100

    add login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,18 +24,23 @@
     </ul>
 
     <ul class="flex items-center">
+      @auth
       <li>
         <a href="" class="p-3">User</a>
       </li>
       <li>
-        <a href="" class="p-3">Login</a>
+        <a href="" class="p-3">Logout</a>
+      </li>
+      @endauth
+
+      @guest
+      <li>
+        <a href="{{ route('login')}}" class="p-3">Login</a>
       </li>
       <li>
         <a href="{{ route('register')}}" class="p-3">Register</a>
       </li>
-      <li>
-        <a href="" class="p-3">Logout</a>
-      </li>
+      @endguest
     </ul>
   </nav>
   @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
 Route::get('/', function () {
     return view('welcome');
 });
